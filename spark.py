e810cde7709961833ffc74ec10233c3cd2fba285
Antoine Lecoffre <antoine.lecoffre@osmose-solutions.com>
2023-10-19 16:01:24 +0200

    Update Spark.py

--- spark.py
+++ spark.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 import re
 from pyspark.sql.types import *
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, udf
 
 spark = SparkSession.builder \
     .appName("MyApp") \
